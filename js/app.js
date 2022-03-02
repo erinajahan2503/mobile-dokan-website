a55575636b377a8ddf9df708be98eb2121fb512e
erina jahan <erinajahan3216@gmail.com>
2022-03-02 10:42:35 +0600

    third commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const allphones = () => {
+    const searchValue = document.getElementById("search-box").value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+    console.log(url);
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    console.log(searchValue);
+};
