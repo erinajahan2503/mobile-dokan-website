055c963f7a4c79997215a2f68e58f3dc115d6b55
erina jahan <erinajahan3216@gmail.com>
2022-03-02 22:12:22 +0600

    last commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
+const main = document.getElementById("main");
 const allphones = () => {
     document.getElementById("phone-container").innerHTML = "";
-    // document.getElementById("spinner").style.display = "block";
+    const error = document.getElementById("error");
     const searchValue = document.getElementById("search-box").value;
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
@@ -11,31 +12,44 @@ const allphones = () => {
 
 };
 const showphoneDetails = (phones) => {
+
+    console.log(phones);
     for (const phone of phones) {
-        const parent = document.getElementById("phone-container");
-
+        console.log(phone);
         const div = document.createElement("div");
-        div.innerHTML = `<div class="card border p-5">
-            <div class="pro-pic w-25 ">
-                  <img class="w-50" src="${phone.image}"class="card-img-top w-50" alt="...">
-              <div class="card-body">
-              <h2 class="phone-title">${phone.phone_name}</h2>
-              <h5>${phone.slug}</h5>
-              <p class="card-text">${phone.brand}</p>
-                 <button onclick="details('${phone.slug}')" class="btn btn-success">Details</button>
-                
-              </div>
-              </div>
-          </div>`;
-        parent.appendChild(div);
-        // console.log(phone);
+        div.classList.add("col-lg-4")
+        div.classList.add("mb-5")
+        div.innerHTML = `
+            <div class="card" style="width: 18rem;">
+                <img class="w-50" src="${phone.image}"class="card-img-top w-50" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.slug}</p>
+                    <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">See Details</button>
+                </div>
+            </div>
+        `
+        main.appendChild(div)
     }
-};
-// ${phone.mainFeatures}
-const details = (id) => {
-    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => console.log(data.data));
-
+}
+const phoneDetails = (slug) => {
+    fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+        .then(res => res.json())
+        .then(data => {
+            const phoneShow = data.data;
+            const singlephone = phoneShow.find(phone => phone.slug === slug)
+            const div = document.createElement("div");
+            main.innerHTML = "";
+            div.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                    <img src="${phone.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${phone.mainFeatures}</h5>
+                        <p class="card-text">${phone.slug}</p>
+                        <p class="card-text">${phone.phone_name}</p>
+                    </div>
+                </div>
+            `
+            main.appendChild(div)
+        })
 };
